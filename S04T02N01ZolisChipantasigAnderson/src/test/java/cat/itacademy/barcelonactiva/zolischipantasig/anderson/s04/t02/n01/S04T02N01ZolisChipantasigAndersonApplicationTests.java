b12776ff86def67192870b517b1812f6810d96ff
anderson92zolis <azolis92@hotmail.es>
2023-03-30 12:04:28 +0200

    generating project spring boot in start.spring.io

---
+++ S04T02N01ZolisChipantasigAnderson/src/test/java/cat/itacademy/barcelonactiva/zolischipantasig/anderson/s04/t02/n01/S04T02N01ZolisChipantasigAndersonApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.zolischipantasig.anderson.s04.t02.n01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T02N01ZolisChipantasigAndersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
