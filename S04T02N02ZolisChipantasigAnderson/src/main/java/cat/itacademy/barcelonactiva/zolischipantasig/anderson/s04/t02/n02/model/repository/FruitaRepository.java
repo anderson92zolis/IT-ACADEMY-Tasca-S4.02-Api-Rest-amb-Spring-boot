ec74f8b9a7ced074d36cd145460d33f7b1e9226a
anderson92zolis <azolis92@hotmail.es>
2023-04-04 18:28:36 +0200

    creating the connecion but someting wrong let chek letter

---
+++ S04T02N02ZolisChipantasigAnderson/src/main/java/cat/itacademy/barcelonactiva/zolischipantasig/anderson/s04/t02/n02/model/repository/FruitaRepository.java
@@ -0,0 +1,10 @@
+package cat.itacademy.barcelonactiva.zolischipantasig.anderson.s04.t02.n02.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import cat.itacademy.barcelonactiva.zolischipantasig.anderson.s04.t02.n02.model.domain.Fruita;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface FruitaRepository extends JpaRepository<Fruita, Integer> {
+}
